3e3810d5772b322d6c13d16c8209f7c636254a85
Manan Tank <manantankm@gmail.com>
2024-01-27 03:54:31 +0530

    update to v4

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,10 @@
 import { ConnectWallet } from '@thirdweb-dev/react';
-import CodeSnippet from '../components/DemoCodeSnippet';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
 	return (
 		<div className={styles.container}>
-			<div className={styles.header}>
-				<h1>Connect Wallet Button</h1>
-				<p style={{ marginBottom: 24 }}>
-					Allow users to connect to your app with the wallet of their choice.
-				</p>
-			</div>
-
-			<ConnectWallet
-				theme='dark'
-				dropdownPosition={{
-					align: 'center',
-					side: 'bottom',
-				}}
-			/>
-
-			<hr className={`${styles.divider} ${styles.spacerTop}`} />
-
-			<h2>Code Snippet</h2>
-
-			<CodeSnippet
-				text={`\
-<ConnectWallet
-  theme='dark'
-  dropdownPosition={{
-    align: 'center',
-    side: 'bottom',
-  }}
-/>`}
-			/>
+			<ConnectWallet theme='dark' />
 		</div>
 	);
 }
